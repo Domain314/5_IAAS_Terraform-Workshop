0040b83cc52177532cb69904e57bbab8351d46f5
Domain314 <domain_69@hotmail.de>
2023-11-29 14:04:45 +0100

    terraform1

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   region = "us-east-1"
-  access_key = var.AWS_ACCESS_KEY
-  secret_key = var.AWS_SECRET_KEY
-  token = var.AWS_TOKEN
+  access_key = secrets.AWS_ACCESS_KEY
+  secret_key = secrets.AWS_SECRET_KEY
+  token = secrets.AWS_TOKEN
 }
 
 # Create a VPC
